b8f607b66c1366dda56abb7a0d86ce379dec7ed1
eduardonba1 <142735616+eduardonba1@users.noreply.github.com>
2023-09-20 20:25:14 -0300

    nvidia

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+os.system(f"apt search nvidia-driver")
+os.system(f"sudo apt install nvidia-driver-525 nvidia-dkms-525")
 os.chdir(f"/home/xlab-app-center")
 os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /home/xlab-app-center/stable-diffusion-webui")
 os.chdir(f"/home/xlab-app-center/stable-diffusion-webui")
