8e1e70ff7bad6cb2cd6a610a4ad643a09995baae
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-10-10 14:56:05 +0530

    Update header.py

--- header.py
+++ header.py
@@ -1,7 +1,7 @@
         with gr.Group():
             gr.Markdown(f'''
-              🐣 Please follow me for new updates https://twitter.com/camenduru
-              🔥 Please join our discord server https://discord.gg/k5BwmmvJJU
-              🥳 Please join my patreon community https://patreon.com/camenduru
+              🐣 This is only for 512X
+              🔥 
+              🥳 
             ''')
 
