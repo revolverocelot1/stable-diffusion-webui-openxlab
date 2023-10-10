e20078463f9fde367029a235547b7923ebdeca70
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-10-10 14:58:01 +0530

    Update header.py

--- header.py
+++ header.py
@@ -1,7 +1,7 @@
         with gr.Group():
             gr.Markdown(f'''
-              🐣 This is only for 512X
-              🔥 
+              🐣 This is only for 512X512
+               CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update
               🥳 
             ''')
 
