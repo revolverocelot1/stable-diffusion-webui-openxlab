1e66a0af43c8d2d620fa573baba03a99eda1182f
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-10-17 02:21:01 +0530

    Update header.py

--- header.py
+++ header.py
@@ -1,5 +1,6 @@
         with gr.Group():
             gr.Markdown(f'''
+              !!TEST APP!! UNSTABLITY IS A CERTENITY. USE https://openxlab.org.cn/apps/detail/ninjawick/stable_diffusion_contorlnet as main
               🐣 This is only for 512X512
                CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update
               🥳 
