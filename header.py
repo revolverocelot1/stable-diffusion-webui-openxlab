ae1826c4ce57de94d4ff9fe64ec87aab50a72faa
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-10-16 16:21:49 +0530

    Update header.py

--- header.py
+++ header.py
@@ -3,5 +3,6 @@
               🐣 This is only for 512X512
                CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update
               🥳 
+              main app depo: https://openxlab.org.cn/apps/detail/ninjawick/stable_diffusion_contorlnet
             ''')
 
