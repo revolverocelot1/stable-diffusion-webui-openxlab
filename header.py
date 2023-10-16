844bf44284ebc272a0c024ddc0dfac6a8647adb8
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-10-16 18:36:44 +0530

    Update header.py

--- header.py
+++ header.py
@@ -1,6 +1,6 @@
         with gr.Group():
             gr.Markdown(f'''
-              🐣 This is only for 512X512
+              🐣 This is only for 512X512, do not attempt to use sdxl, it will crash
                CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update
               🥳 
             ''')
