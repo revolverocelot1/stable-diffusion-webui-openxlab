9bfeec1292f65b82146e7424400926f5e4d1d7ff
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-12-11 15:30:36 +0530

    Update header.py

--- header.py
+++ header.py
@@ -1,7 +1,7 @@
         with gr.Group():
             gr.Markdown(f'''
-              🐣 This is only for 512X512, do not attempt to use sdxl, it will crash
-               CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update
+              🐣 This is only for 512X models, do not attempt to change model while genreating, it will crash
+               CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update and bugs
               🥳 
             ''')
 
