243d1690d45fa2aad3e1f082ba9715ecd92f9c69
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2025-07-31 22:41:07 +0530

    Add files via upload

--- header.py
+++ header.py
@@ -1,7 +1,8 @@
         with gr.Group():
             gr.Markdown(f'''
-              🐣 This is only for 512X models, do not attempt to change model while genreating, it will crash
-               CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update and bugs
-               huggingface links has problems nowadays,so model migration is in progress on this platfrom.
+              🐣 This is only for 512X512
+               CONTACT ON https://github.com/revolverocelot1 or srushtiraj.patil20@vit.edu for extenstion update
+              🥳 
+              main app depo: https://openxlab.org.cn/apps/detail/ninjawick/stable_diffusion_contorlnet
             ''')
 
